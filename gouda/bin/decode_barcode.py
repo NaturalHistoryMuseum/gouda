791f128a384bfb2e74e24b31608ac4ff7386417b
Lawrence Hudson <quicklizard@googlemail.com>
2015-01-06 14:56:36 +0000

    Added option to rename files

--- gouda/bin/decode_barcode.py
+++ gouda/bin/decode_barcode.py
@@ -7,6 +7,7 @@ import glob
 import os
 import sys
 import time
+import traceback
 
 from functools import partial
 
@@ -17,6 +18,7 @@ import gouda.util
 from gouda.engines import (AccusoftEngine, DataSymbolEngine,
                            InliteEngine, LibDMTXEngine, StecosEngine,
                            SoftekEngine, ZbarEngine, ZxingEngine)
+from gouda.gouda_error import GoudaError
 from gouda.util import expand_wildcard, read_image
 from gouda.strategies import roi, resize
 
@@ -24,27 +26,38 @@ from gouda.strategies import roi, resize
 # TODO LH Visitor that copies file p to a new file for each decoded barcode
 #         value (see Chris' process)
 
-def decode(paths, strategies, engine, visitor, read_greyscale):
+def decode(paths, strategies, engine, visitors, read_greyscale):
     """ Finds and decodes barcodes in the image at the given path.
     """
     for p in paths:
         if p.is_dir():
-            decode(p.iterdir(), strategies, engine, visitor, read_greyscale)
+            decode(p.iterdir(), strategies, engine, visitors, read_greyscale)
         else:
             # TODO LH Read greyscale affects decoders?
             # TODO LH Command-line argument for greyscale / colour
-            img = read_image(p, read_greyscale)
-            if img is None:
-                visitor.result(p, [None, []])
-            else:
-                for strategy in strategies:
-                    result = strategy(img, engine)
-                    if result:
-                        break
-                visitor.result(p, result)
+            try:
+                img = read_image(p, read_greyscale)
+                if img is None:
+                    for visitor in visitors:
+                        visitor.result(p, [None, []])
+                else:
+                    for strategy in strategies:
+                        result = strategy(img, engine)
+                        if result:
+                            break
+                    if not result:
+                        result = [None, []]
+
+                    for visitor in visitors:
+                        visitor.result(p, result)
+            except Exception:
+                print('Error processing [{0}]'.format(p))
+                traceback.print_exc()
 
 
-class BasicReporter(object):
+class BasicReportVisitor(object):
+    """Writes a line-per-file and a line-per-barcode to stdout
+    """
     def result(self, path, result):
         print(path)
         strategy, barcodes = result
@@ -53,16 +66,20 @@ class BasicReporter(object):
             print('[{0}] [{1}] [{2}]'.format(index, barcode.type, barcode.data))
 
 
-class TerseReporter(object):
+class TerseReportVisitor(object):
+    """Writes a line-per-file to stdout
+    """
     def result(self, path, result):
         strategy, barcodes = result
         values = [b.data for b in barcodes]
         print(path, ' '.join(['[{0}]'.format(v) for v in values]))
 
 
-class CSVReporter(object):
-    def __init__(self, engine, greyscale):
-        self.w = csv.writer(sys.stdout)
+class CSVReportVisitor(object):
+    """Writes a CSV report
+    """
+    def __init__(self, engine, greyscale, file=sys.stdout):
+        self.w = csv.writer(file)
         self.w.writerow(['OS','Engine','Directory','File','Image.conversion',
                          'Elapsed','N.found','Values','Strategy'])
         self.engine = engine
@@ -71,19 +88,42 @@ class CSVReporter(object):
 
     def result(self, path, result):
         strategy, barcodes = result
-        decoded = ['[{0}:{1}]'.format(b.type, b.data) for b in barcodes]
+        decoded = [u'[{0}:{1}]'.format(b.type, b.data) for b in barcodes]
         decoded = u' '.join(decoded)
         self.w.writerow([sys.platform,
                          self.engine,
                          path.parent.name,
-                         path.stem,
+                         path.name,
                          self.image_conversion,
                          time.time()-self.start_time,
                          len(barcodes),
                          decoded,
                          strategy])
 
+class RenameVisitor(object):
+    """Renames files based on their barcodes
+    """
+    def result(self, path, result):
+        strategy, barcodes = result
+        print(path)
+        if not barcodes:
+            print('  No barcodes')
+        else:
+            barcodes = [b.data.replace('(', '-').replace(')', '') for b in barcodes]
+            fname = '_'.join(barcodes)
+            dest = path.parent / (fname + path.suffix)
+            if path == dest:
+                print('  Already correctly named')
+            elif dest.is_file():
+                print('  Cannot rename to [{0}] because destination exists'.format(dest))
+            else:
+                path.rename(dest)
+                print('  Renamed to [{0}]'.format(dest))
+
 def engine_choices():
+    """Returns a dict mapping command-line options to functions that return an
+    engine
+    """
     choices = {'libdmtx': LibDMTXEngine,
                'zbar': ZbarEngine,
                'zxing': ZxingEngine,
@@ -125,7 +165,7 @@ if __name__=='__main__':
 
     parser = argparse.ArgumentParser(description='Finds and decodes barcodes on images')
     parser.add_argument('--debug', '-v', action='store_true')
-    parser.add_argument('--report', '-r', choices=['basic', 'terse', 'csv'], default='basic')
+    parser.add_argument('--action', '-r', choices=['basic', 'terse', 'csv', 'rename'], default='basic')
     parser.add_argument('--greyscale', '-g', action='store_true')
 
     choices = engine_choices()
@@ -141,13 +181,15 @@ if __name__=='__main__':
 
     engine = choices[args.engine]()
 
-    if 'csv'==args.report:
-        reporter = CSVReporter(args.engine, args.greyscale)
-    elif 'terse'==args.report:
-        reporter = TerseReporter()
+    if 'csv'==args.action:
+        visitor = CSVReportVisitor(args.engine, args.greyscale)
+    elif 'terse'==args.action:
+        visitor = TerseReportVisitor()
+    elif 'rename'==args.action:
+        visitor = RenameReporter()
     else:
-        reporter = BasicReporter()
+        visitor = BasicReportVisitor()
 
     strategies = [resize, roi]
-    decode(expand_wildcard(args.image), strategies, engine, reporter,
+    decode(expand_wildcard(args.image), strategies, engine, [visitor],
            args.greyscale)

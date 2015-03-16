bce974f75c4841e8182505a4671061976617f55a
Lawrence Hudson <quicklizard@googlemail.com>
2015-03-16 17:31:26 +0000

    Simpler Barcode representation

--- gouda/barcode.py
+++ gouda/barcode.py
@@ -5,10 +5,4 @@ from gouda.gouda_error import GoudaError
 class Barcode(collections.namedtuple('Barcode', ['type', 'data'])):
     """A simple representation of a barcode
     """
-    def __new__(cls, type, data):
-        if not type:
-            raise GoudaError('No type')
-        elif not data:
-            raise GoudaError('No data')
-        else:
-            return super(Barcode, cls).__new__(cls, type, data)
+    pass

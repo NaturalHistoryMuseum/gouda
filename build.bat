b2f25fac8d9e42e794977035eca471c29cc6414a
Lawrence Hudson <quicklizard@googlemail.com>
2015-07-28 10:11:58 +0100

    Remove trailing whitespace

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ REM Temporary solution until I get round to writing a makefile
 echo Clean
 del /S *pyc
 del *spec
-rmdir /Q /S dist build 
+rmdir /Q /S dist build
 
 echo Tests
 nosetests --with-coverage --cover-html --cover-inclusive --cover-erase --cover-tests --cover-package=gouda

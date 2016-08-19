6488ad3b5a1e2d419711d43dcc785786b0ab6be3
Lawrence Hudson <quicklizard@googlemail.com>
2016-08-19 09:42:07 +0100

    Don't clear spec files

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e  # Exit on failure
 echo Clean
 find . -name "*pyc" -print0 | xargs -0 rm -rf
 find . -name __pycache__ -print0 | xargs -0 rm -rf
-rm -rf *spec dist build cover
+rm -rf dist build cover
 
 echo Tests
 nosetests --with-coverage --cover-html --cover-inclusive --cover-erase --cover-tests --cover-package=gouda
